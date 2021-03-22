0dad4cef138410fd80af62ad069dd64291fd1446
A_S_T_E_R_1_S_K_ <donhon05@gmail.com>
2021-03-22 11:48:55 +0900

    Update a~bplus.py
    while true: 문 추가.

--- a~bplus.py
+++ a~bplus.py
@@ -5,26 +5,27 @@ b = 0
 result = 0
 
 def when_start():
-    a = 0
-    b = 0
-    result = 0
-    Entry.input("첫번째 1부터 10까지의 숫자 입력")
-    a = Entry.answer()
-    if a > 10:
-        Entry.print_for_sec("10보다 큰 숫자입니다.", 5)
-        break
-    if a <= 0:
-        Entry.print_for_sec("1보다 작은 숫자입니다", 5)
-        break
-    Entry.input("두번째 1부터 10까지의 숫자 입력")
-    b = Entry.answer()
-    if b > 10:
-        Entry.print_for_sec("10보다 큰 숫자입니다.", 5)
-        break
-    if b <= 0:
-        Entry.print_for_sec("1보다 작은 숫자입니다", 5)
-        break
-    while a <= b:
-        result = result + a
-        a += 1
-    Entry.print_for_sec(("합계 " + result), 5)
+    while True:
+        a = 0
+        b = 0
+        result = 0
+        Entry.input("첫번째 1부터 10까지의 숫자 입력")
+        a = Entry.answer()
+        if a > 10:
+            Entry.print_for_sec("10보다 큰 숫자입니다.", 5)
+            break
+        if a <= 0:
+            Entry.print_for_sec("1보다 작은 숫자입니다", 5)
+            break
+        Entry.input("두번째 1부터 10까지의 숫자 입력")
+        b = Entry.answer()
+        if b > 10:
+            Entry.print_for_sec("10보다 큰 숫자입니다.", 5)
+            break
+        if b <= 0:
+            Entry.print_for_sec("1보다 작은 숫자입니다", 5)
+            break
+        while a <= b:
+            result = result + a
+            a += 1
+        Entry.print_for_sec(("합계 " + result), 5)
