9fe33be6640705153915790deb56515a2d24be97
ASTER18K <donhon05@gmail.com>
2021-06-01 20:49:39 +0900

    0601
    1학기 수업 중 만든 파일 백업 추가 및 이외 자료 추가.

---
+++ ETC2-EntryPy-Renshu/pyvsmod/1.py
@@ -0,0 +1,7 @@
+i = 0
+sum = 0
+
+for i in range(1, 11, 1):
+    sum = sum+1
+
+print("%d" % sum)
---
+++ ETC2-EntryPy-Renshu/pyvsmod/2.py
@@ -0,0 +1,9 @@
+i = 0
+sum = 0
+
+i = 1
+while i < 11:
+    sum = sum + i
+    i = i + 1
+
+print("%d" % sum)
